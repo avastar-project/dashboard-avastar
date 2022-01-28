99073a87af31296aacac562ee78a85cae7fc60d1
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2022-01-28 16:02:41 +0100

    Remove temporary links on each page after MainNav component creation

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,7 @@
-import { Link } from 'react-router-dom';
-
 export default function Homepage() {
   return (
     <div>
       <h1>Homepage</h1>
-      <nav>
-        <Link to="overview">Overview</Link>
-      </nav>
     </div>
   );
 }
