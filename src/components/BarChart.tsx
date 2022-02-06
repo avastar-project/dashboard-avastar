31e9d23a38ca5f1b166e10d7b49af6bc2cbe8261
tangigouez <45219966+tangigouez@users.noreply.github.com>
2022-02-06 22:33:40 +0100

    add fake source file and template viz

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,30 @@
+import Plot from 'react-plotly.js';
+
+import data from '../fake-data/fake_data_agg.json'
+
+const CountLocation = data.data_classification.filter(data => data.data_type == 'Location').length
+const CountBehavioural = data.data_classification.filter(data => data.data_type == 'Behavioural').length
+const CountCommunication = data.data_classification.filter(data => data.data_type == 'Communication').length
+const CountTechnical = data.data_classification.filter(data => data.data_type == 'Technical').length
+const CountSocialRelationship = data.data_classification.filter(data => data.data_type == 'Social relationship').length
+const CountContact = data.data_classification.filter(data => data.data_type == 'Contact').length
+const CountTransactional = data.data_classification.filter(data => data.data_type == 'Transactional').length
+
+console.log(CountTransactional)
+
+
+const BarChart = () => {
+
+    return (
+    <Plot
+      data={[
+        {type: 'bar', 
+        x: ['Location', 'Behavioural', 'Communication', 'Technical', 'Social relationship', 'Contact', 'Transactional'], 
+        y: [CountLocation, CountBehavioural, CountCommunication, CountTechnical, CountSocialRelationship, CountContact, CountTransactional], marker: {color : ['#636EFA','#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692']}},
+      ]}
+      layout={{width: 800, height: 450, title: 'What is being tracked ?', paper_bgcolor : 'rgba(0,0,0,0)', plot_bgcolor : 'rgba(0,0,0,0)', yaxis : {title : {text : 'Number of data points', font : {size: 13}}}, xaxis: {categoryorder: 'total descending'}}}
+    />
+    );
+  }
+
+  export default BarChart
