6f2e7a1690274167cefd3ffbcd633628739592c4
CaroleFeelGood <46852629+CaroleFeelGood@users.noreply.github.com>
2022-03-30 21:54:08 -0400

    adapt TrackedCahrt for fake_data_agg

--- src/components/Overview/PlotlyCharts/TrackedChart.tsx
+++ src/components/Overview/PlotlyCharts/TrackedChart.tsx
@@ -1,35 +1,44 @@
-// Example for how to create a plotly component
-import Plotly from 'plotly.js-basic-dist';
-import createPlotlyComponent from 'react-plotly.js/factory';
+import Plotly from "plotly.js";
+import createPlotlyComponent from "react-plotly.js/factory";
+import * as data_aggregat from '../../../fake-data/fake_data_agg.json'
+
+//Doc link for layout references : https://plotly.com/javascript/reference
 const Plot = createPlotlyComponent(Plotly);
 
-export default function TrackedChart() {
-  return (
-    <Plot
-      data={[
-        {
-          x: [
-            'Locational',
-            'Behavioural',
-            'Communications',
-            'Technical',
-            'Social relationships',
-            'Contact',
-            'Transactional',
-            'Financial',
-            'Socio-demographic',
-            'Contractual',
-          ],
-          y: [295, 275, 185, 95, 90, 75, 45, 25, 10, 5],
-          textposition: 'auto',
-          type: 'bar',
-          mode: 'lines+markers',
-          marker: { cmin: 0, cmax: 255 },
-        },
-      ]}
-      // Doc link for layout references : https://plotly.com/javascript/reference/#layout
-      layout={{ autosize: true, title: 'Volume of data points' }}
-      config={{ responsive: true }}
-    />
-  );
+export default function LineChart() {
+  interface Anything {
+      [key: string]: any;
+  }
+  var plotType: Plotly.PlotType = 'bar'
+
+  var counts: Anything = {};
+  
+  let getData = () =>{
+    data_aggregat.data_classification.forEach(object => {
+      counts[object.data_type] = (counts[object.data_type] || 0) + 1;
+    })
+    return counts
+  } 
+
+  let data = getData()
+
+  const dataPlot = [
+    {
+      x: Object.keys(data),
+      y: Object.values(data),
+      type: plotType,
+      marker: { cmin: 0, cmax: 255 }
+    },
+  ];
+  const layout = {
+    autosize: true,
+    title: {
+      text: 'Volume of data points',
+      // yref: 'paper',
+      // y: 0.9,
+      }
+  }
+
+
+  return <Plot data={dataPlot} layout={layout} />;
 }
