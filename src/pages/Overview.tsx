ea95bf0e8befb2dfc4b5fd704e4a3836d56941aa
npfeffer <n.pfeffer@cafeyn.co>
2022-03-31 23:55:01 +0200

    removing first dropzone

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,10 +1,7 @@
-import DropzoneMultiplefiles from '../dropZone'
-
 export default function Overview() {
   return (
     <div>
       <h1>Overview</h1>
-      <DropzoneMultiplefiles />
     </div>
   );
 }
