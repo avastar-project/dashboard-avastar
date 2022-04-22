7836c9d719f00b4269578cff9dfac5ccb4845af4
Saulnier <l.saulnier@outlook.com>
2022-04-22 11:54:25 +0200

    refacto Mui StepsUploadDataSection component

--- src/components/Home/StepsUploadDataSection.tsx
+++ src/components/Home/StepsUploadDataSection.tsx
@@ -5,6 +5,8 @@
 //utils
 import styled from 'styled-components';
 
+// MUI components
+import { Box,Typography } from '@mui/material';
 // Icons
 import RequestDataIcon from '../../assets/icon-request-data.jpg';
 import UploadDataIcon from '../../assets/icon-upload-data.png';
@@ -87,25 +89,22 @@ const StepItem = styled.li`
 display:flex;
 justify-content:center;
 align-items:center;
-gap:2rem;
+gap:4rem;
 list-style-type:none;
 width:100%;
 padding:1rem 2rem;
-
- & > div {
-   display:flex;
-   width:50%;
-
-  & > img {
-    object-fit: cover;
-    width: 21rem;
-    height: auto;
-    margin: auto;
-    }
 }
 `;
+const ImgContainer = styled(Box)`
+width:50%;
 
-const Content = styled.ul`
+& > img {
+  object-fit: cover;
+  width:50%
+  }
+`;
+
+const ContentContainer = styled.ul`
 display:flex;
 flex-direction:column;
 gap:1rem;
@@ -127,7 +126,7 @@ width:50%;
   top: 0;
   left: 0;
   width: 15%;
-  border-top: 0.4rem solid var(--clr-blue);
+  border-top: 0.4rem solid var(--clr-blue-primary);
 }
 
 & > li:last-child {
@@ -143,15 +142,15 @@ export default function StepsUploadData() {
           {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */}
         {steps.map((steps:Steps, index:number) => (
           <StepItem key={index}>
-            <div>
+            <ImgContainer display='flex' justifyContent='center'>
              <img src={steps.icon} alt={`icon-${steps.name}`} /> 
-            </div>
-            <Content>
-             <li>{steps.name}</li>
-             <li>{steps.content.p1}</li>
-             <li>{steps.content.p2}</li>
-             <li>{steps.content.pItalic}</li>
-            </Content>
+            </ImgContainer>
+            <ContentContainer>
+             <li><Typography variant='h4'>{steps.name}</Typography></li>
+             <li><Typography variant='body1'>{steps.content.p1}</Typography></li>
+             <li><Typography variant='body1'>{steps.content.p2}</Typography></li>
+             <li><Typography variant='body1'>{steps.content.pItalic}</Typography></li>
+            </ContentContainer>
           </StepItem>
         ))}
       </StepsList>
