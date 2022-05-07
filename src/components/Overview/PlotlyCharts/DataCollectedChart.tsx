32285d5d34a0a2db0d03c44cd642524a38fdea1c
tangigouez <45219966+tangigouez@users.noreply.github.com>
2022-05-07 15:21:50 +0200

    connect charts with state data

--- src/components/Overview/PlotlyCharts/DataCollectedChart.tsx
+++ src/components/Overview/PlotlyCharts/DataCollectedChart.tsx
@@ -1,15 +1,43 @@
 // Example for how to create a plotly component
 import Plotly from 'plotly.js-basic-dist';
 import createPlotlyComponent from 'react-plotly.js/factory';
+import { useSelector, shallowEqual } from 'react-redux';
+import {
+  AvastarParsedDataPoint,
+  AvastarParsedDataPointState,
+} from '../../../types/dataTypes';
+
 const Plot = createPlotlyComponent(Plotly);
 
 export default function DataCollectedChart() {
+  // Fetch data from State
+  const avastarParsedData: readonly AvastarParsedDataPoint[] = useSelector(
+    (state: AvastarParsedDataPointState) => state.avastarParsedData,
+    shallowEqual
+  );
+  // Aggregate the count of each data type
+  let observedCount = 0;
+  let volunteeredCount = 0;
+  let inferredCount = 0;
+  let otherCount = 0;
+  for (let i = 0; i < avastarParsedData.length; i++) {
+    if (avastarParsedData[i].data_origin == 'observed') {
+      observedCount++;
+    } else if (avastarParsedData[i].data_origin == 'volunteered') {
+      volunteeredCount++;
+    } else if (avastarParsedData[i].data_origin == 'inferred') {
+      inferredCount++;
+    } else {
+      otherCount++;
+    }
+  }
+
   return (
     <Plot
       data={[
         {
           x: ['Observed', 'Volunteered', 'Inferred'],
-          y: [40, 25, 10],
+          y: [observedCount, volunteeredCount, inferredCount],
           textposition: 'auto',
           type: 'bar',
           mode: 'lines+markers',
--- src/components/Overview/PlotlyCharts/TrackedChart.tsx
+++ src/components/Overview/PlotlyCharts/TrackedChart.tsx
@@ -1,9 +1,58 @@
 // Example for how to create a plotly component
 import Plotly from 'plotly.js-basic-dist';
 import createPlotlyComponent from 'react-plotly.js/factory';
+import { useSelector, shallowEqual } from 'react-redux';
+import {
+  AvastarParsedDataPoint,
+  AvastarParsedDataPointState,
+} from '../../../types/dataTypes';
+
 const Plot = createPlotlyComponent(Plotly);
 
 export default function TrackedChart() {
+  // Fetch data from State
+  const avastarParsedData: readonly AvastarParsedDataPoint[] = useSelector(
+    (state: AvastarParsedDataPointState) => state.avastarParsedData,
+    shallowEqual
+  );
+  // Aggregate the count of each data type
+  let locationalCount = 0;
+  let behaviouralCount = 0;
+  let communicationCount = 0;
+  let technicalCount = 0;
+  let socialRelationshipCount = 0;
+  let contactCount = 0;
+  let transactionalCount = 0;
+  let financialCount = 0;
+  let socioDemographicCount = 0;
+  let contractualCount = 0;
+  let otherCount = 0;
+  for (let i = 0; i < avastarParsedData.length; i++) {
+    if (avastarParsedData[i].data_type == 'location') {
+      locationalCount++;
+    } else if (avastarParsedData[i].data_type == 'behavioural') {
+      behaviouralCount++;
+    } else if (avastarParsedData[i].data_type == 'communications') {
+      communicationCount++;
+    } else if (avastarParsedData[i].data_type == 'technical') {
+      technicalCount++;
+    } else if (avastarParsedData[i].data_type == 'social relationships') {
+      socialRelationshipCount++;
+    } else if (avastarParsedData[i].data_type == 'contact') {
+      contactCount++;
+    } else if (avastarParsedData[i].data_type == 'transactional') {
+      transactionalCount++;
+    } else if (avastarParsedData[i].data_type == 'financial') {
+      financialCount++;
+    } else if (avastarParsedData[i].data_type == 'socio-demographic') {
+      socioDemographicCount++;
+    } else if (avastarParsedData[i].data_type == 'contractual') {
+      contractualCount++;
+    } else {
+      otherCount++;
+    }
+  }
+
   return (
     <Plot
       data={[
@@ -20,7 +69,18 @@ export default function TrackedChart() {
             'Socio-demographic',
             'Contractual',
           ],
-          y: [295, 275, 185, 95, 90, 75, 45, 25, 10, 5],
+          y: [
+            locationalCount,
+            behaviouralCount,
+            communicationCount,
+            technicalCount,
+            socialRelationshipCount,
+            contactCount,
+            transactionalCount,
+            financialCount,
+            socioDemographicCount,
+            contractualCount,
+          ],
           textposition: 'auto',
           type: 'bar',
           mode: 'lines+markers',
