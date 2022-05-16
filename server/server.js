16c3f3b452986e1e98b5823e4f3d41b9b788f60c
npfeffer <n.pfeffer@cafeyn.co>
2022-05-16 23:07:54 +0200

    react app ready to deploy

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
 
 app.get('/*', (req, res) => {
