941bde49cac1f196005dfaf92fc82343e437247d
npfeffer <n.pfeffer@cafeyn.co>
2022-05-16 23:30:33 +0200

    add comment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 
+// to be changed build to public in development mode
 const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
 
