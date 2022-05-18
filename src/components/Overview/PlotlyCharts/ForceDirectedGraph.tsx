1125497c1f529fda98d8695da6a58df9872331e9
tangigouez <45219966+tangigouez@users.noreply.github.com>
2022-05-19 00:26:10 +0200

    push first version of the force graph

---
+++ src/components/Overview/PlotlyCharts/ForceDirectedGraph.tsx
@@ -0,0 +1,56 @@
+import { Datum } from 'plotly.js';
+import React from 'react';
+import {
+  ForceGraph2D,
+  ForceGraph3D,
+  ForceGraphVR,
+  ForceGraphAR,
+} from 'react-force-graph';
+
+import myData from '../../../fake-data/force-directed-graph-fake-data.json';
+
+export default function TrackedChart() {
+  const GROUPS = 12;
+  return (
+    <ForceGraph2D
+      graphData={myData}
+      nodeAutoColorBy="group"
+      linkDirectionalParticles="value"
+      linkDirectionalParticleSpeed={(d: any) => d.value * 0.001}
+      linkWidth={1}
+      nodeCanvasObject={(node: any, ctx: any, globalScale: any) => {
+        const label = node.id;
+        const fontSize = 12 / globalScale;
+        ctx.font = `${fontSize}px Sans-Serif`;
+        const textWidth = ctx.measureText(label).width;
+        const bckgDimensions = [textWidth, fontSize].map(
+          (n) => n + fontSize * 0.2
+        ); // some padding
+
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        ctx.fillRect(
+          node.x - bckgDimensions[0] / 2,
+          node.y - bckgDimensions[1] / 2,
+          ...bckgDimensions
+        );
+
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle = node.color;
+        ctx.fillText(label, node.x, node.y);
+
+        node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+      }}
+      nodePointerAreaPaint={(node: any, color: any, ctx: any) => {
+        ctx.fillStyle = color;
+        const bckgDimensions = node.__bckgDimensions;
+        bckgDimensions &&
+          ctx.fillRect(
+            node.x - bckgDimensions[0] / 2,
+            node.y - bckgDimensions[1] / 2,
+            ...bckgDimensions
+          );
+      }}
+    />
+  );
+}
