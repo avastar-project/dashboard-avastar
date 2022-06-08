c022e00e22ef35cb42072c8e9abc3203c3cf4f60
tangigouez <45219966+tangigouez@users.noreply.github.com>
2022-06-08 14:03:58 +0200

    resolve conflicts stepper

--- src/components/Home/StepperSection.tsx
+++ src/components/Home/StepperSection.tsx
@@ -265,13 +265,23 @@ function getStepContent(step: number) {
 export default function MultiStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
+  // const handleNext = () => {
+  //   if (activeStep < 2) {
+  //     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  //   } else {
+  //     navigate('/overview');
+  //   }
+  // };
+
+  let navigate = useNavigate();
 
   const handleNext = () => {
-    if (activeStep < 2) {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    } else {
+    if (activeStep === steps.length - 1) {
       navigate('/overview');
+    } else {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
     }
   };
 
