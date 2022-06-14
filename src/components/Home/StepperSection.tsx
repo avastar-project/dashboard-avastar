8afe4f97c67837b941147c8e2f67c3654421bd6c
Tangi Gouez <tangi.gouez@dashlane.com>
2022-06-15 00:30:25 +0200

    add demo video

--- src/components/Home/StepperSection.tsx
+++ src/components/Home/StepperSection.tsx
@@ -247,7 +247,7 @@ function getStepContent(step: number) {
           <Box width={'100%'}>
             <video width="100%" height="auto" controls>
               <source
-                src={require('../../assets/video/video-test.mp4')}
+                src={require('../../assets/video/webapp-preview.mp4')}
                 type="video/mp4"
               />
             </video>
